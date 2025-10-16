e07565000d84f7b1886d238fba2decd63226cff1
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2025-10-16 14:11:46 -0400

    feat: update Mapbox token retrieval to use environment variable
    - Removed hardcoded Mapbox token from the codebase.
    - Updated getPublicMapboxToken function to return the token from environment variables, enhancing security and flexibility.

--- code/frontend/src/component/map/getPublicMapboxToken.ts
+++ code/frontend/src/component/map/getPublicMapboxToken.ts
@@ -1,6 +1,3 @@
-export const PUBLIC_MAPBOX_TOKEN =
-  "pk.eyJ1IjoiamJla2VsZSIsImEiOiJjbWdzdWY5Ym4wMG5sMmtwcTRhYzRhOWtjIn0.TKG7ko0aFtYgXoiyATBIxg";
-
 export function getPublicMapboxToken(): string {
-  return "pk.eyJ1IjoiamJla2VsZSIsImEiOiJjbWdzdWY5Ym4wMG5sMmtwcTRhYzRhOWtjIn0.TKG7ko0aFtYgXoiyATBIxg";
+  return process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "";
 }
