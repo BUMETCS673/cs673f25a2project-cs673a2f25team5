defbbb3f14de4c2c400bf0ff017cc097d5f530a4
saniyaab <saniyabhambhvani@gmail.com>
2025-11-03 18:05:27 -0500

    eslint failed test

--- code/frontend/src/component/events/EventFilter.tsx
+++ code/frontend/src/component/events/EventFilter.tsx
@@ -11,7 +11,7 @@ const sortEvents = [
 ];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 export function EventFilter() {
